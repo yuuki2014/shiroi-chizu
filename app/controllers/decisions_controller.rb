cde466ca49d391d0a78f9f4d13ceebcf095b8a3b
増子祐貴 <masukoyuki@mashikoyuukinoMacBook-Air.local>
2025-12-29 19:02:29 +0900

    lint修正

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -3,15 +3,15 @@ class DecisionsController < ApplicationController
     context  = params[:context]
     decision = params[:decision]
 
-    case [context, decision]
-    when ["tutorials", "end"]
+    case [ context, decision ]
+    when [ "tutorials", "end" ]
       cookies.permanent[:tutorials_end] = "true"
       respond_to do |format|
         format.turbo_stream { render turbo_stream: turbo_stream.update("modal-container", partial: "tutorials/terms_modal") }
         format.html { redirect_to root_path }
       end
 
-    when ["terms", "accepted"]
+    when [ "terms", "accepted" ]
       cookies.permanent[:terms_accepted] = "true"
 
       # ゲストユーザー作成
@@ -26,7 +26,7 @@ class DecisionsController < ApplicationController
         format.html { redirect_to root_path }
       end
 
-    when ["terms", "dismissed"]
+    when [ "terms", "dismissed" ]
       respond_to do |format|
         format.turbo_stream { render "tutorials/terms_required_modal", formats: :turbo_stream }
         format.html { redirect_to root_path }
